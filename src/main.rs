a6e35b233c50b184ef418c5453462ce4d4437aa9
Siddhartha <asthana.siddhartha@gmail.com>
2024-09-12 10:19:26 +0530

    check changed url

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main()  {
-    println!("hello world");
+    println!("hello worldss");
+    println!("guess the number");
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("failed to read line");
+
+    println!(" you guessed {}" , guess)
+    
+
 }
